ca9ce7e6045b84b51974c0f7eac371e7ebbd726d
charamedicharla26 <charanmedicharla06@gmail.com>
2025-08-09 15:21:28 +0530

    Update Linux_jenkins.sh

--- Linux_jenkins.sh
+++ Linux_jenkins.sh
@@ -8,10 +8,3 @@ sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo mkdir -p /var/tmp_disk
-sudo chmod 1777 /var/tmp_disk
-sudo mount --bind /var/tmp_disk /tmp
-echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
-sudo systemctl mask tmp.mount
-df -h /tmp
-sudo systemctl restart jenkins
